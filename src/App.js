9a34893ac1f7cd8dbd639ca5247b7ea8731e365a
Vincent Liu <unicorn610@gmail.com>
2020-09-14 20:18:41 +1000

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Homepage from './pages/Homepage'
+import './default.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div class="main">
+        <Homepage />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
